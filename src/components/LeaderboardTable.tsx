ab6563490d9d7f00659102c87f9181941123d195
UrosD <30302460+Uros47@users.noreply.github.com>
2023-12-24 10:41:40 +0100

    no data logic

--- src/components/LeaderboardTable.tsx
+++ src/components/LeaderboardTable.tsx
@@ -25,7 +25,7 @@ const LeaderboardTable = () => {
   const [rowsPerPage, setRowsPerPage] = useState<number>(10);
   const [totalCount, setTotalCount] = useState<number>(0);
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
-  const [sortColumn, setSortColumn] = useState<string>("Date of creation");
+  const [sortColumn, setSortColumn] = useState<string>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [searchData, setSearchData] = useState<string>("");
 
@@ -41,9 +41,7 @@ const LeaderboardTable = () => {
     try {
       const queryParams = `?q=${searchData}&_page=${
         currentPage + 1
-      }&_limit=${rowsPerPage}&_sort=${
-        sortColumn === "Date of creation" ? "createdAt" : sortColumn
-      }&_order=${sortOrder}`;
+      }&_limit=${rowsPerPage}&_sort=${sortColumn}&_order=${sortOrder}`;
 
       const users = await fetch(
         `${process.env.NEXT_PUBLIC_API}/users` + queryParams,
@@ -112,62 +110,90 @@ const LeaderboardTable = () => {
       <Box alignSelf="end" sx={{ marginBottom: "10px" }}>
         <Search searchData={searchData} setSearchData={setSearchData} />
       </Box>
-      <TableContainer sx={{ maxHeight: "603px" }} component={Paper}>
-        <Table
-          sx={{
-            margin: "0 auto",
-            width: "1200px",
-          }}
-          stickyHeader
-          aria-label="collapsible table"
-        >
-          <TableHead>
-            <TableRow>
-              {cellTitles.map((title: string, index: number) => (
-                <TableCell
-                  sx={{ fontWeight: 600 }}
-                  key={index}
-                  sortDirection={sortColumn === title ? sortOrder : false}
-                >
-                  {title !== "Date of creation" ? (
-                    title
-                  ) : (
+      {tableData.length > 0 ? (
+        <>
+          {" "}
+          <TableContainer sx={{ maxHeight: "603px" }} component={Paper}>
+            <Table
+              sx={{
+                margin: "0 auto",
+                width: "1200px",
+              }}
+              stickyHeader
+              aria-label="collapsible table"
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell sx={{ fontWeight: 600 }} sortDirection={sortOrder}>
                     <TableSortLabel
-                      active={sortColumn === "Date of creation"}
+                      active={true}
                       direction={sortOrder}
-                      onClick={() => handleSortRequest("Date of creation")}
+                      onClick={() => handleSortRequest("name")}
                     >
-                      {title}
+                      User
                     </TableSortLabel>
-                  )}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {tableData.length > 0 &&
-              tableData.map((item: any) => (
-                <TableRow key={item.id}>
-                  <TableCell>{item.name}</TableCell>
-                  <TableCell>{formatter.format(item.profit)}</TableCell>
-                  <TableCell>{formatter.format(item.loss)}</TableCell>
-                  <TableCell style={{ color: `${item.balance < 0 && "red"}` }}>
-                    {formatter.format(item.balance)}
+                  </TableCell>
+                  <TableCell sx={{ fontWeight: 600 }} sortDirection={sortOrder}>
+                    <TableSortLabel
+                      active={true}
+                      direction={sortOrder}
+                      onClick={() => handleSortRequest("profit")}
+                    >
+                      Profit
+                    </TableSortLabel>
+                  </TableCell>
+                  <TableCell sx={{ fontWeight: 600 }} sortDirection={sortOrder}>
+                    <TableSortLabel
+                      active={true}
+                      direction={sortOrder}
+                      onClick={() => handleSortRequest("loss")}
+                    >
+                      Loss
+                    </TableSortLabel>
+                  </TableCell>
+                  <TableCell
+                    sx={{ fontWeight: 600 }}
+                    sortDirection={sortColumn === "Balance" ? sortOrder : false}
+                  >
+                    <TableSortLabel
+                      active={sortColumn === "Balance"}
+                      direction={sortOrder}
+                      onClick={() => handleSortRequest("balance")}
+                    >
+                      Balance
+                    </TableSortLabel>
                   </TableCell>
                 </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 15, 20]}
-        component="div"
-        count={totalCount}
-        rowsPerPage={rowsPerPage}
-        page={currentPage}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
+              </TableHead>
+              <TableBody>
+                {tableData.map((item: any) => (
+                  <TableRow key={item.id}>
+                    <TableCell>{item.name}</TableCell>
+                    <TableCell>{formatter.format(item.profit)}</TableCell>
+                    <TableCell>{formatter.format(item.loss)}</TableCell>
+                    <TableCell
+                      style={{ color: `${item.balance < 0 && "red"}` }}
+                    >
+                      {formatter.format(item.balance)}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 15, 20]}
+            component="div"
+            count={totalCount}
+            rowsPerPage={rowsPerPage}
+            page={currentPage}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </>
+      ) : (
+        <Typography>No data to display.</Typography>
+      )}
     </>
   );
 };
